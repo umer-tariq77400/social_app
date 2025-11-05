ef0abdbd82cac6bf62d597878429ae996e9c1d92
[umer-tariq77400] <[umert77400@gmail.com]>
2025-11-05 22:29:42 +0500

    Refactor authentication views and templates: streamline login/logout URLs, implement password reset functionality, and remove unused login view. Switched to django auth framework for authentication

--- accounts/views.py
+++ accounts/views.py
@@ -8,24 +8,3 @@ from .forms import LoginForm
 @login_required
 def dashboard(request):
     return render(request, 'accounts/dashboard.html', {"section": "dashboard"})
-
-
-def login_view(request):   
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse("Login successful.")
-                else:
-                    return HttpResponse("Account disabled.")
-            else:
-                return HttpResponse("Invalid credentials.")
-    else:
-        form = LoginForm()
-    
-    return render(request, 'accounts/login.html', {'form': form})
