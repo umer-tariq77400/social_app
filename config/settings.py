efa51b22b643a152186a65260fba8a598f8685ad
[umer-tariq77400] <[umert77400@gmail.com]>
2025-11-26 15:36:54 +0500

    Fix: Update Azure Blob Storage to use STORAGES dict (Django 5.2+ pattern)

--- config/settings.py
+++ config/settings.py
@@ -147,12 +147,40 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # WhiteNoise configuration for efficient static file serving
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"    
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# Media files settings
+if DEBUG:
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
+else:
+    # Production: Azure Blob Storage
+    AZURE_ACCOUNT_NAME = config("AZURE_STORAGE_ACCOUNT_NAME", default="")
+    AZURE_ACCOUNT_KEY = config("AZURE_STORAGE_ACCOUNT_KEY", default="")
+    AZURE_CONTAINER = config("AZURE_STORAGE_CONTAINER_NAME", default="social-app-media")
+    
+    MEDIA_URL = f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/"
+    
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
+    
+    # Azure Storage configuration
+    AZURE_CUSTOM_DOMAIN = f"{AZURE_ACCOUNT_NAME}.blob.core.windows.net"
+    AZURE_URL_EXPIRATION_SECS = None  # URLs don't expire
 
 # Authentication settings
 AUTHENTICATION_BACKENDS = [
@@ -193,29 +221,9 @@ LOGOUT_URL = "logout"
 #  Email configuration for password reset
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# Media files settings
-if DEBUG:
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
-else:
-    # Azure Blob Storage configuration for production
-    AZURE_ACCOUNT_NAME = config("AZURE_STORAGE_ACCOUNT_NAME", default="")
-    AZURE_ACCOUNT_KEY = config("AZURE_STORAGE_ACCOUNT_KEY", default="")
-    AZURE_CONTAINER = config("AZURE_STORAGE_CONTAINER_NAME", default="")
-    
-    # Azure SDK settings for django-storages
-    AZURE_STORAGE_CONNECTION_STRING = config("AZURE_STORAGE_CONNECTION_STRING", default="")
-    AZURE_STORAGE_CUSTOM_DOMAIN = config("AZURE_STORAGE_CUSTOM_DOMAIN", default="")
-    
-    # Build MEDIA_URL from custom domain if available, otherwise use account name
-    if AZURE_STORAGE_CUSTOM_DOMAIN and AZURE_CONTAINER:
-        MEDIA_URL = f"https://{AZURE_STORAGE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/"
-    else:
-        MEDIA_URL = f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/"
-    
-    # Configure django-storages for Azure Blob Storage
-    DEFAULT_FILE_STORAGE = "storages.backends.azure_storage.AzureStorage"
-
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Easy Thumbnails configuration
 THUMBNAIL_DEBUG = DEBUG
