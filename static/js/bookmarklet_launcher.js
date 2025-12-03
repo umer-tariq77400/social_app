9811e64c09ff3b41343cfb0c4a5a490e7fcd7132
[umer-tariq77400] <[umert77400@gmail.com]>
2025-12-03 10:57:02 +0500

    change the url of bookmarket

--- static/js/bookmarklet_launcher.js
+++ static/js/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function(){
     if(!window.bookmarklet){
         const bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        bookmarklet_js.src = `http://localhost:8000/static/js/bookmarklet.js?r=${Math.random() * 999999999}`;
+        bookmarklet_js.src = `https://docker-app-9a76b3f1350c.herokuapp.com/static/js/bookmarklet.js?r=${Math.random() * 999999999}`;
         window.bookmarklet = true;
     }
     else {
